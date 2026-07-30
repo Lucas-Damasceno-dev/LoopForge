6589f7814442abeaf6b54c0b52fd7d375bd2de4a
Lucas Damasceno <lucas.damasceno.dev@gmail.com>
2026-07-30 01:59:51 -0300

    checkpoint: loopforge/task-run-9a0b1c06-f600-42f1-bf64-5bcb37af413f

--- packages/registry/registry/__init__.py
+++ packages/registry/registry/__init__.py
@@ -1,3 +1,22 @@
 """Agentic Interface Registry package."""
 
+from .core.checker import RegistryChecker
+from .core.scanner import InterfaceScanner
+from .store.sqlite import RegistryStore
+from .notifier.stdout import StdoutNotifier
+from .notifier.slack import SlackNotifier
+from .notifier.file import FileNotifier
+from .store.models import InterfaceItem, BreakingChange, RegistrySchema
+
 __version__ = "0.1.0"
+__all__ = [
+    "RegistryChecker",
+    "InterfaceScanner",
+    "RegistryStore",
+    "StdoutNotifier",
+    "SlackNotifier",
+    "FileNotifier",
+    "InterfaceItem",
+    "BreakingChange",
+    "RegistrySchema",
+]
--- packages/retro/retro/__init__.py
+++ packages/retro/retro/__init__.py
@@ -1,3 +1,22 @@
 """Agentic Retro package."""
 
+from .core.parser import AgDRParser
+from .core.analyzer import SessionAnalyzer
+from .core.recommender import Recommender
+from .report.renderer import RetroRenderer
+from .store.sqlite import RetroStore
+from .store.models import SessionRecord, RetroReport, PatternItem, LearningItem, AgDREvent
+
 __version__ = "0.1.0"
+__all__ = [
+    "AgDRParser",
+    "SessionAnalyzer",
+    "Recommender",
+    "RetroRenderer",
+    "RetroStore",
+    "SessionRecord",
+    "RetroReport",
+    "PatternItem",
+    "LearningItem",
+    "AgDREvent",
+]
